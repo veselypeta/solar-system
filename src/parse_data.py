3f1b82f77386f663e5a8f6260429315b69872e3b
petrvesely <petr.vesely@skyscanner.net>
2019-09-17 14:23:44 +0100

    working version

--- src/parse_data.py
+++ src/parse_data.py
@@ -9,10 +9,10 @@ def parse_planet_data(filename):
         all_planets = []
         for planet in csv_reader:
             name = planet[0]
-            mass = float(planet[1])
-            position = np.array([float(planet[2]), float(planet[3])])
-            velocity = np.array([float(planet[4]), float(planet[5])])
-            radius = float(planet[6])
+            mass = np.float64(planet[1])
+            position = np.array([np.float64(planet[2]), np.float64(planet[3])])
+            velocity = np.array([np.float64(planet[4]), np.float64(planet[5])])
+            radius = np.float64(planet[6])
             colour = str(planet[7])
             p = Planet(name, mass, position, velocity, radius, colour)
             all_planets.append(p)
--- src/simulation.py
+++ src/simulation.py
@@ -9,13 +9,45 @@ def get_animation_range(planets):
     return np.amax([np.linalg.norm(p.position) for p in planets])
 
 
-def next_position(planet):
-    pass
+def next_position(p):
+    return p.position + (p.velocity * k.TIME_STEP) + \
+           ((1.0/6.0 * k.TIME_STEP * k.TIME_STEP) * ((4.0 * p.acceleration) - p.previous_acceleration))
+
+
+def next_velocity(p, next_acceleration):
+    return p.velocity + ((1.0/6.0)*(2.0*next_acceleration + 5.0*p.acceleration - p.previous_acceleration) * k.TIME_STEP)
+
+
+def normalise_vector(v):
+    mag = np.linalg.norm(v)
+    if mag == 0:
+        return v
+    return v/mag
+
+
+def force_gravity(p, planets):
+    force = 0
+    for P in planets:
+        if p != P:
+            distance = np.linalg.norm(p.position - P.position)
+            force_mag = (k.G * p.mass * P.mass) / distance**2
+            direction_vector = normalise_vector(P.position - p.position)
+            force += force_mag * direction_vector
+
+    return force
 
 
 def run_time_step(planets):
-    # calculate the next position
-    next_positions = [next_position(p) for p in planets]
+    # update positions of planets
+    for p in planets:
+        p.position = next_position(p)
+    next_accelerations = [force_gravity(p, planets)/p.mass for p in planets]
+    next_velocities = [next_velocity(p, next_accelerations[i]) for i, p in enumerate(planets)]
+    # update the values for acceleration & velocity
+    for i, p in enumerate(planets):
+        p.velocity = next_velocities[i]
+        p.previous_acceleration = p.acceleration
+        p.acceleration = next_accelerations[i]
 
 
 def move_planets(i, patches, planets):
@@ -33,6 +65,11 @@ def run_simulation():
     planets = parse_planet_data('src/solar_data.csv')
     fig = plt.figure()
     ax = plt.axes()
+
+    # initialise accelerations for first time_step
+    for p in planets:
+        p.acceleration = force_gravity(p, planets)/p.mass
+        p.previous_acceleration = p.acceleration
 
     ax.axis('scaled')
     # set the range of the plot
